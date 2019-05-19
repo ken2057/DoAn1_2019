e05041b08dd0406fb8b367136205796cec052451
ken2057 <50286742+ken2057@users.noreply.github.com>
2019-05-19 20:34:25 +0700

    promotion test success

--- DA_BookStore/DA_BookStore/Controllers/Test2Controller.cs
+++ DA_BookStore/DA_BookStore/Controllers/Test2Controller.cs
@@ -1,21 +1,23 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 
 namespace DA_BookStore.Controllers
 {
     public class Test2Controller : ApiController
     {
-        public IEnumerable<Models.QUANGCAO> Get()
-        {
-            using (var db = new Models.BookStore())
-            {
-                return db.QUANGCAOs.ToList();
-            }
-        }
+        //public IEnumerable<Models.QUANGCAO> Get()
+        //{
+        //    using (var db = new Models.BookStore())
+        //    {
+        //        return db.QUANGCAOs.ToList();
+        //    }
+        //}
         // GET: api/Test2/5
         public string Get(int id)
         {
@@ -23,9 +25,7 @@ namespace DA_BookStore.Controllers
         }
 
         // POST: api/Test2
-        public void Post([FromBody]string value)
-        {
-        }
+        //public void Post([FromBody]string value){}
 
         // PUT: api/Test2/5
         public void Put(int id, [FromBody]string value)
@@ -37,6 +37,30 @@ namespace DA_BookStore.Controllers
         {
         }
 
-       
+        
+        public string Get()
+        {
+            using (var db = new Models.BookStore())
+            {
+                List<Models.Temp.PromoView> listPromo = new List<Models.Temp.PromoView>();
+
+
+                db.KHUYENMAIs.ToList()
+                        .ForEach(t => listPromo
+                                        .Add( new Models.Temp.PromoView(t.TenKhuyenMai, t.NgayBatDau, t.NgayKetThuc) ));
+
+                JsonSerializerSettings microsoftDateFormatSettings = new JsonSerializerSettings
+                {
+                    DateFormatHandling = DateFormatHandling.MicrosoftDateFormat
+                };
+
+                var grpPromo = new Models.Temp.Group { group = "A", data = listPromo };
+                var listGrp = new List<Models.Temp.Group>() { grpPromo };
+                //var json = JsonConvert.SerializeObject(listGrp, microsoftDateFormatSettings);
+                var json = JsonConvert.SerializeObject(listGrp);
+                return json;
+                return json.Replace("\"\\/Date(", "Date(").Replace(")\\/\"", ")");
+            }
+        }
     }
 }
---
+++ DA_BookStore/DA_BookStore/Models/Temp/PromotionView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DA_BookStore.Models.Temp
+{
+   
+
+    public class PromoView
+    {
+        public string label { get; set; }
+        public List<object> data { get; set; }
+
+        private class tempPromo
+        {
+            public List<DateTime> timeRange;
+            public string var = "1";
+
+            public tempPromo(DateTime? bd, DateTime? kt)
+            {
+                timeRange = new List<DateTime>();
+                timeRange.Add(bd ?? DateTime.Now);
+                timeRange.Add(kt ?? DateTime.Now);
+            }
+        }
+
+        public PromoView(string lb, DateTime? bd, DateTime? kt)
+        {
+            label = lb;
+            data = new List<object>() { new tempPromo(bd,kt) };
+        }
+    }
+
+    public class Group
+    {
+        public string group { get; set; }
+        public List<PromoView> data { get; set; }
+    }
+}
