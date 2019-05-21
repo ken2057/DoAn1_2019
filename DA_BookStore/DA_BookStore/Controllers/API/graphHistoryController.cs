3d45ac0723127f66d5a6c2d9510e8ed52ea8176f
ken2057 <50286742+ken2057@users.noreply.github.com>
2019-05-21 12:51:50 +0700

    change word cloud to bubble chart

--- DA_BookStore/DA_BookStore/Controllers/API/graphHistoryController.cs
+++ DA_BookStore/DA_BookStore/Controllers/API/graphHistoryController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web;
 
 namespace DA_BookStore.Controllers.API
 {
@@ -12,26 +13,71 @@ namespace DA_BookStore.Controllers.API
     {
         public string Get()
         {
-            using(var db = new Models.BookStore())
+            return queryData();
+        }
+        public string Get(int day)
+        {
+            return queryData(day);
+        }
+
+        private string queryData(int day = 30)
+        {
+            using (var db = new Models.BookStore())
             {
                 List<object> grp = new List<object>();
                 Dictionary<string, double> dctCT = new Dictionary<string, double>();
+                var minDay = DateTime.Now.AddDays(-day);
 
-                foreach (var item in db.CTXEMSACHes.ToList())
-                    if (!dctCT.Keys.Contains(item.MaSach))
-                        dctCT.Add(item.MaSach, 1);
+                var query = from ctxem in db.CTXEMSACHes
+                            join s in db.SACHes on ctxem.MaSach equals s.MaSach
+                            where ctxem.NgayXemSach >= minDay
+                            select new { s.TenSach, s.SoLuongTon, s.MaSach };
+
+                foreach (var item in query)
+                {
+                    if (!dctCT.Keys.Contains( item.MaSach+" "+item.TenSach+" ("+item.SoLuongTon+")"))
+                        dctCT.Add(item.MaSach + " " + item.TenSach + " (" + item.SoLuongTon + ")", 1);
                     else
-                        dctCT[item.MaSach] += 1;
+                        dctCT[item.MaSach + " " + item.TenSach + " (" + item.SoLuongTon + ")"] += 1;
+                }
+
+                var test = new classTemp();
 
                 foreach (var item in dctCT)
-                    grp.Add(new List<object> { item.Key, item.Value/dctCT.Values.Max() });
+                {
+                    var text = item.Key.Split(' ').ToArray();
+                    var sl = text[text.Count() - 1];
+                    var id = text[0];
+                    var name = item.Key.Replace(" " + sl, "")
+                                        .Replace(id + " ", " ");
+                    sl = sl.Replace("(", "").Replace(")", "");
+                    test.children.Add(new classTemp.temp(name, item.Value, int.Parse(sl), id));
+                }
 
-
-                var json = JsonConvert.SerializeObject(grp);
-                return json;
+                return JsonConvert.SerializeObject(test);
             }
-
-           
         }
+
+        private class classTemp
+        {
+            public List<temp> children { get; set; }
+
+            public classTemp() { children = new List<temp>(); }
+
+            public class temp
+            {
+                public string Name { get; set; }
+                public double Count { get; set; }
+                public double SoLuong { get; set; }
+                public string id { get; set; }
+
+                public temp(string name, double counts, int sl, string id)
+                {
+                    Name = name; Count = counts; SoLuong = sl;this.id = id;
+                }
+            }
+        }
+
+        
     }
 }
