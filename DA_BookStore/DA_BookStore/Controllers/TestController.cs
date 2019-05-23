1ec98371249d41fedffe0192e5c000d1c7ae002c
ken2057 <50286742+ken2057@users.noreply.github.com>
2019-05-23 13:16:56 +0700

    add loom chart

--- DA_BookStore/DA_BookStore/Controllers/TestController.cs
+++ DA_BookStore/DA_BookStore/Controllers/TestController.cs
@@ -19,15 +19,16 @@ namespace DA_BookStore.Controllers
             }
             return View();
         }
-
-        public string GraphPromo()
+        [HttpGet]
+        public string GraphPromo(int day = 30)
         {
             using (var db = new Models.BookStore())
             {
                 List<Models.Temp.PromoView> listPromo = new List<Models.Temp.PromoView>();
+                DateTime timeUp = DateTime.Now.AddDays(day);
+                DateTime timeDown = DateTime.Now.AddDays(-day);
 
-
-                db.KHUYENMAIs.ToList()
+                db.KHUYENMAIs.Where(t => t.NgayKetThuc < timeUp && t.NgayBatDau > timeDown).ToList()
                         .ForEach(t => listPromo
                                         .Add(new Models.Temp.PromoView(t.TenKhuyenMai, t.NgayBatDau, t.NgayKetThuc, t.PhanTramKhuyenMai ?? 0)));
 
@@ -38,7 +39,7 @@ namespace DA_BookStore.Controllers
 
             }
         }
-
+        [HttpGet]
         public string GraphHistory(int day = 30)
         {
             using (var db = new Models.BookStore())
@@ -74,6 +75,133 @@ namespace DA_BookStore.Controllers
                 test.children.Sort();
 
                 return JsonConvert.SerializeObject(test);
+            }
+        }
+        [HttpGet]
+        public string GraphGroupDataCustomer()
+        {
+            using (var db = new Models.BookStore())
+            {
+                // get customer and total bill
+                var cus = db.HOADONMUAHANGs.GroupBy(t => t.TenTaiKhoan)
+                    .Select(t => new
+                    {
+                        TenTK = t.Select(g => g.TenTaiKhoan).FirstOrDefault(),
+                        TongTien = t.Sum(g => g.TongTien)
+                    }).OrderBy(t => t.TongTien).ToList();
+
+                // group customer via total bill
+                Dictionary<string, int> grp1 = new Dictionary<string, int>(); // below 200k
+                Dictionary<string, int> grp2 = new Dictionary<string, int>(); // 200k - 1.000k
+                Dictionary<string, int> grp3 = new Dictionary<string, int>(); // > 1.000k
+                foreach (var item in cus)
+                {
+                    if (item.TongTien >= 1000000)
+                        grp3.Add(item.TenTK, item.TongTien ?? -1);
+                    else if (item.TongTien >= 200000)
+                        grp2.Add(item.TenTK, item.TongTien ?? -1);
+                    else
+                        grp1.Add(item.TenTK, item.TongTien ?? -1);
+                }
+                List<object> test = new List<object>() { grp1, grp2, grp3 };
+
+                //get list book per group customer
+                var lstTemp = db.CTHOADONMUAHANGs.Where(t => grp1.Keys.Contains(t.HOADONMUAHANG.TenTaiKhoan))
+                    .Select(t => new { TL1 = t.SACH.MaTL1, TL2 = t.SACH.MaTL2, TL3 = t.SACH.MaTL3 }).ToList();
+                Dictionary<string, int> lst1 = new Dictionary<string, int>();
+                foreach (var item in lstTemp)
+                {
+                    if (item.TL1 != null)
+                    {
+                        if (lst1.Keys.Contains(item.TL1))
+                            lst1[item.TL1] += 1;
+                        else
+                            lst1.Add(item.TL1, 1);
+                    }
+                    if (item.TL2 != null)
+                    {
+                        if (lst1.Keys.Contains(item.TL2))
+                            lst1[item.TL2] += 1;
+                        else
+                            lst1.Add(item.TL2, 1);
+                    }
+                    if (item.TL3 != null)
+                    {
+                        if (lst1.Keys.Contains(item.TL3))
+                            lst1[item.TL3] += 1;
+                        else
+                            lst1.Add(item.TL3, 1);
+                    }
+                }
+
+                lstTemp = db.CTHOADONMUAHANGs.Where(t => grp2.Keys.Contains(t.HOADONMUAHANG.TenTaiKhoan))
+                   .Select(t => new { TL1 = t.SACH.MaTL1, TL2 = t.SACH.MaTL2, TL3 = t.SACH.MaTL3 }).ToList();
+                Dictionary<string, int> lst2 = new Dictionary<string, int>();
+                foreach (var item in lstTemp)
+                {
+                    if (item.TL1 != null)
+                    {
+                        if (lst2.Keys.Contains(item.TL1))
+                            lst2[item.TL1] += 1;
+                        else
+                            lst2.Add(item.TL1, 1);
+                    }
+                    if (item.TL2 != null)
+                    {
+                        if (lst2.Keys.Contains(item.TL2))
+                            lst2[item.TL2] += 1;
+                        else
+                            lst2.Add(item.TL2, 1);
+                    }
+                    if (item.TL3 != null)
+                    {
+                        if (lst2.Keys.Contains(item.TL3))
+                            lst2[item.TL3] += 1;
+                        else
+                            lst2.Add(item.TL3, 1);
+                    }
+                }
+
+                lstTemp = db.CTHOADONMUAHANGs.Where(t => grp3.Keys.Contains(t.HOADONMUAHANG.TenTaiKhoan))
+                                    .Select(t => new { TL1 = t.SACH.MaTL1, TL2 = t.SACH.MaTL2, TL3 = t.SACH.MaTL3 }).ToList();
+                Dictionary<string, int> lst3 = new Dictionary<string, int>();
+                foreach (var item in lstTemp)
+                {
+                    if (item.TL1 != null)
+                    {
+                        if (lst3.Keys.Contains(item.TL1))
+                            lst3[item.TL1] += 1;
+                        else
+                            lst3.Add(item.TL1, 1);
+                    }
+                    if (item.TL2 != null)
+                    {
+                        if (lst3.Keys.Contains(item.TL2))
+                            lst3[item.TL2] += 1;
+                        else
+                            lst3.Add(item.TL2, 1);
+                    }
+                    if (item.TL3 != null)
+                    {
+                        if (lst3.Keys.Contains(item.TL3))
+                            lst3[item.TL3] += 1;
+                        else
+                            lst3.Add(item.TL3, 1);
+                    }
+                }
+
+                List<Dictionary<string, int>> grpListTheLoaiXem = new List<Dictionary<string, int>>() { lst1, lst2, lst3 };
+                List<Models.Temp.GroupCusView> test2 = new List<Models.Temp.GroupCusView>();
+
+                foreach (var item in grpListTheLoaiXem)
+                {
+                    foreach (var t in item)
+                    {
+                        test2.Add(new Models.Temp.GroupCusView("Group "+(grpListTheLoaiXem.IndexOf(item)+1), t.Key, t.Value));
+                    }
+                }
+
+                return JsonConvert.SerializeObject(test2);
             }
         }
 
